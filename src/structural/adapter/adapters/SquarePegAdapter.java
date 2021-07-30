b67f5bbeeffb0b68bf38bd05c53572c43f53c9e1
joceano <alves.joceano@gmail.com>
2021-07-30 18:16:01 -0300

    Adapter

---
+++ src/structural/adapter/adapters/SquarePegAdapter.java
@@ -0,0 +1,24 @@
+package structural.adapter.adapters;
+
+import structural.adapter.round.RoundPeg;
+import structural.adapter.square.SquarePeg;
+
+/**
+ * Adapter allows fitting square pegs into round holes.
+ */
+public class SquarePegAdapter extends RoundPeg  {
+
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg) {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double result;
+        // Calculate a minimum circle radius, which can fit this peg.
+        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+        return result;
+    }
+}

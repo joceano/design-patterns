89c25cff7a9b631d865604b5db1d9a03ec36f0d5
joceano <alves.joceano@gmail.com>
2021-07-30 14:39:58 -0300

    Builder

---
+++ src/creational/builder/builders/Builder.java
@@ -0,0 +1,19 @@
+package creational.builder.builders;
+
+import creational.builder.cars.CarType;
+import creational.builder.components.Engine;
+import creational.builder.components.GPSNavigator;
+import creational.builder.components.Transmission;
+import creational.builder.components.TripComputer;
+
+/**
+ * Builder interface defines all possible ways to configure a product.
+ */
+public interface Builder {
+    void setCarType(CarType type);
+    void setSeats(int seats);
+    void setEngine(Engine engine);
+    void setTransmission(Transmission transmission);
+    void setTripComputer(TripComputer tripComputer);
+    void setGPSNavigator(GPSNavigator gpsNavigator);
+}
